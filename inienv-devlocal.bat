bb977826235ab4bb62b305d3135722a87b345d94
André Luís <33747658+andreluisvsa@users.noreply.github.com>
2023-05-16 03:40:48 -0300

    Comentários em inienv-devlocal.bat

--- inienv-devlocal.bat
+++ inienv-devlocal.bat
@@ -1,2 +1,6 @@
 @echo off
+@REM 
+@REM   Inicia shell como usuário devlocal. Caso seja a primeira vez,
+@REM   solicitará a senha.
+
 runas /user:devlocal /savedcred "cmd /d /e:ON /k d:\env\setenv-devlocal.bat"
