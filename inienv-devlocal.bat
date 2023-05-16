abc2f3fa0dc8ba35c20cafb9363603df16c9c70f
André Luís <33747658+andreluisvsa@users.noreply.github.com>
2023-05-16 03:43:57 -0300

    Update inienv-devlocal.bat

--- inienv-devlocal.bat
+++ inienv-devlocal.bat
@@ -3,4 +3,4 @@
 @REM   Inicia shell como usuário devlocal. Caso seja a primeira vez,
 @REM   solicitará a senha.
 
-runas /user:devlocal /savedcred "cmd /d /e:ON /k d:\env\setenv-devlocal.bat"
+RUNAS /user:devlocal /savedcred "cmd /d /e:ON /k d:\env\setenv-devlocal.bat"
